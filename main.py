d27f88e2c4a129fe4ddb2e6acb8f6a4b791228af
Tezigudo <godjangg@gmail.com>
2022-08-28 14:12:35 +0700

    Finished work

--- main.py
+++ main.py
@@ -1,39 +1,42 @@
+from models import Item, Arithematics
 from http.client import HTTPException
 from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
-from models import Item
 
 NAME = os.getenv('NAME', default="World")
 
 app = FastAPI()
 
-# TODO: 1.) Add endpoint as a top-level page.
-@app.get("")
+
+@app.get("/")
 def index():
     return {"message": f"Hello {NAME}"}
 
-# TODO: 2.) Create a calculator that accept 2 query params and manipulate those 2 values.
-@app.get("/calculator/add")
-def adder(first_number: int, second_number: int):
-    # TODO: Start here
-    return {"first_number": first_number, "second_number": second_number, "result": 0}
+
+@app.get("/calculator/add/")
+def adder(num: Arithematics):
+    return {"first_number": num.first_number, "second_number": num.second_number, "result": num.first_number+num.second_number}
+
 
 @app.get("/calculator/substract")
-def subtractor(first_number: int, second_number: int):
-    # TODO: Start here
-    return {"first_number": first_number, "second_number": second_number, "result": 0}
-
-# TODO: (Optional) @app.get("/calculator/multiply")
-
-# TODO: (Optional) @app.get("/calculator/divide")
+def subtractor(num: Arithematics):
+    return {"first_number": num.first_number, "second_number": num.second_number, "result": num.first_number-num.second_number}
 
 
+@app.get("/calculator/multiply")
+def multiply(num: Arithematics):
+    return {"first_number": num.first_number, "second_number": num.second_number, "result": num.first_number*num.second_number}
 
-# TODO: 3.) Enter a path scheme that accept userId as a path parameter `userId`.
-@app.get("")
+
+@app.get("/calculator/divide")
+def divide(num: Arithematics):
+    return {"first_number": num.first_number, "second_number": num.second_number, "result": num.first_number/num.second_number}
+
+
+@app.get("/{userId}")
 def usersInfo(userId: int):
     if userId != 9997:
         raise HTTPException(status_code=404, detail="UserId not found.")
@@ -45,15 +48,12 @@ def usersInfo(userId: int):
     }
 
 
-# TODO: 4.) Make this method to accept `POST` request and request body on path `/item/create/`.
 # Example Request Body:
 # {
 #   "name": "Book",
 #   "price": 123,
 # }
-# @app.????(???)
+@app.post("/item/create/")
 def itemCreator(item: Item):
-    # TODO: Print body's name and price to console.
+    print(item.name, item.price)
     return item.name, item.price
-
-
--- models.py
+++ models.py
@@ -5,3 +5,7 @@ from typing import Union
 class Item(BaseModel):
     name: str
     price: float
+
+class Arithematics(BaseModel):
+    first_number: float
+    second_number: float
